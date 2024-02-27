1561ae5fc561e8db582e384ec3729d38827813ca
egaldamez1 <157499124+egaldamez1@users.noreply.github.com>
2024-02-27 09:20:52 -0600

    Add files via upload

---
+++ semana 7/T1_EGM_1247324/Program.cs
@@ -0,0 +1,23 @@
+﻿class programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi segundo programa");
+
+        Console.WriteLine("Ingresa tu nombre");
+        string sNombre = Console.ReadLine();
+
+         Console.WriteLine("Ingresa tu edad");
+        string sEdad = Console.ReadLine();
+
+         Console.WriteLine("Ingresa tu carrera");
+        string scarrera = Console.ReadLine();
+
+         Console.WriteLine("Ingresa tu carné");
+        string scarne = Console.ReadLine();
+
+        Console.WriteLine("Soy " + sNombre + " tengo " + sEdad + ", años y estudio la carrera de " + scarrera);
+
+        Console.WriteLine("Mi carné es " + scarne);
+    }
+}
