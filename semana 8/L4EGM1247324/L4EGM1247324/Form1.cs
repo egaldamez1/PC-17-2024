0a9e38540520c8cfb4f93d982a97db68db18c30b
egaldamez1 <157499124+egaldamez1@users.noreply.github.com>
2024-03-05 10:03:43 -0600

    Add files via upload

---
+++ semana 8/L4EGM1247324/L4EGM1247324/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L4EGM1247324
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            int n;
+            int resultado;
+            n = int.Parse(label1.Text);
+            ...
+
+            label1.Text = resultado.ToString();
+        }
+    }
+}
